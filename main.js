324f37790614726ae196c458f3b54253710a489a
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2024-02-29 09:26:56 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -2,20 +2,28 @@ var divSaldo = document.getElementById("moedass")
 divSaldo.innerHTML = `<img src="https://i.imgur.com/2ZkJcuv.png" height="30px"> ${saldo}`
 
 function ver() {
-  if (saldo >= 10) {
+  
   
   window.location.href = "videos.html"
-  } else {
+  
     
-    alert("Você não tem saldo suficiente, clique em assistir mais videos...")
+    
     
   }
   
 }
 
 var enviar = () => {
-  
+  if (saldo >= 10) {
+
+saldo = saldo - 10
+localStorage.setItem("saldo",saldo.toString)
+
   window.location.href = "enviar.html"
-  
+  } else {
+ 
+alert("Você não tem saldo, assista videos!")
+
+}
 }
 
