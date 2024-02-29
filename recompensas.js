dbdb9a58452fecc8fb740a194d5803029779ab3f
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2024-02-29 10:57:59 -0300

    Atualizar o recompensas.js

--- recompensas.js
+++ recompensas.js
@@ -1,6 +1,6 @@
 var Daniel = document.getElementById("daniel")
 var saldo = parseInt(localStorage.getItem("saldo"))
-if (!saldo) {
+if (saldo === null) {
   
   saldo = 20
   localStorage.setItem("saldo",saldo)
