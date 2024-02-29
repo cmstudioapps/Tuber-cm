b4fe54a5c7c83fd76cbf8dee7867a866bcd766df
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2024-02-29 14:59:30 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ var saldo = parseInt(localStorage.getItem("saldo"))
 var divSaldo = document.getElementById("moedass")
 divSaldo.innerHTML = `<img src="https://i.imgur.com/2ZkJcuv.png" height="30px"> ${saldo}`
 
+window.addEventListener("load", function() {
+(function(d, w, c) {
+        w.ChatraID = 'LQnrWiXFxKmhKMTta';
+        var s = d.createElement('script');
+        w[c] = w[c] || function() { (w[c].q = w[c].q || []).push(arguments); };
+        s.async = true;
+        s.src = 'https://call.chatra.io/chatra.js';
+        if (d.head) d.head.appendChild(s);
+    })(document, window, 'Chatra');
+    })
+
 function verr() {
   
   
