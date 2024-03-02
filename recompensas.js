e64335bd504074f661c4a11a547ecad55aad6dd7
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2024-03-02 17:03:48 -0300

    Atualizar o recompensas.js

--- recompensas.js
+++ recompensas.js
@@ -1,6 +1,7 @@
 var Daniel = document.getElementById("daniel")
 var saldo = parseInt(localStorage.getItem("saldo"))
-if (saldo === null || saldo == NaN) {
+if (saldo === null || isNaN(saldo)) {
+alert("ocorreu um erro inesperado")
   
   saldo = 20
   localStorage.setItem("saldo",saldo.toString())
